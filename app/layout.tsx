f264e76ca1f41c7471de990d00109d955088bcb6
Edgar-Bello <belloglzedgar@gmail.com>
2024-10-17 12:06:49 -0500

    Super cool Website

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ramiro Santos",
-  description: "Ramiro Santos Website",
+  title: "Edgar Bello",
+  description: "Edgar Website",
 };
 
 export default function RootLayout({
